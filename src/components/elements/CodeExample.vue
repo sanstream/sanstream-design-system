e3ab55a5af6d63914966f2fa71b853bdb2adc166
Sanne Peters <sanstream@pop-os.localdomain>
2020-11-14 14:49:07 +0100

    Patched errors made in cbac342

--- src/components/elements/CodeExample.vue
+++ src/components/elements/CodeExample.vue
@@ -1,12 +1,15 @@
 <template>
   <!--Everything inlined so the example code is rendered withou weird indentations -->
-  <pre :class="`language-${language}`"><code>{{convertedCode}}</code></pre>
+  <pre :class="`language-${language}`">
+    <code class="sanstream-special-text">
+      <span v-html="convertedCode" />
+    </code>
+  </pre>
 </template>
 
 <script>
 import Prism from 'prismjs'
 import VueTypes from 'vue-types'
-Prism.highlightAll()
 
 /**
  * Displays code examples for 'html', 'javascript' and 'css' using
@@ -27,6 +30,12 @@ export default {
     ]),
   },
 
+  mounted () {
+    // Prism needs to interact with the DOM,
+    // which is loaded when the component is mounted:
+    Prism.highlightAll()
+  },
+
   computed: {
     convertedCode () {
       return Prism.highlight(this.code, Prism.languages[this.language], this.language)
@@ -34,6 +43,12 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+code {
+  font-size: 100%;
+}
+</style>
 
 <docs>
 ```jsx
