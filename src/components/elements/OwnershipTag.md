0e380adfc0a6a5b92bb454ce30be6cac7ef6252d
Sanne Peters <sanstream@pop-os.localdomain>
2020-11-27 14:29:24 +0100

    Added OwnershipTag

---
+++ src/components/elements/OwnershipTag.md
@@ -0,0 +1,17 @@
+```jsx
+<OwnershipTag
+  :yearOfCreation="2020"
+/>
+```
+
+As part of a h1. Behaves as inline text by default.
+
+```jsx
+<h1 class="sanstream-heading">
+  A cool new app
+  <br />
+  <OwnershipTag
+    :yearOfCreation="2020"
+  />
+</h1>
+```
---
+++ src/components/elements/OwnershipTag.vue
@@ -0,0 +1,41 @@
+<template>
+  <small class="sanstream-special-text">
+    by
+    <StandardLink
+      href="https://www.sanstream.nl"
+      target="_blank">
+      Studio Sanstream
+    </StandardLink>&nbsp;©&nbsp;{{yearOfCreation}}
+  </small>
+</template>
+
+<script>
+import VueTypes from 'vue-types'
+import StandardLink from './StandardLink'
+
+/**
+ * A way to tag anything not directly on sanstream.nl being owned by me.
+ *
+ * Is always shown in conjuction with a website or tool's name. For
+ * that reason it's size is also reduced.
+ */
+export default {
+  name: 'OwnershipTag',
+
+  components: {
+    StandardLink,
+  },
+
+  props: {
+    yearOfCreation: VueTypes.integer.isRequired,
+  },
+}
+</script>
+
+<style scoped>
+small {
+  font-size: 0.8rem;
+  /* keep it's own weight: */
+  font-weight: initial;
+}
+</style>
