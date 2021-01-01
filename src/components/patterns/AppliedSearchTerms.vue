4b4cfebb4c5ffee1fb95a5a35e86b1cc90f0efbd
Sanne Peters <sanstream@pop-os.localdomain>
2021-01-01 11:22:16 +0100

    Added the AppliedSearchTerms component.

---
+++ src/components/patterns/AppliedSearchTerms.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ol
+      class="search-terms sanstream-no-list-styles"
+      v-if="searchTerms.length"
+    >
+      <li
+        v-for="term in searchTerms"
+        :key="term"
+      >
+        <em
+          class="sanstream-special-text"
+        >{{term}}</em>
+        <Button
+          variant="textual"
+          :onClick="() => {}"
+        >
+          <Icon
+            id="CloseCross"
+            titleText="remove"
+          />
+        </Button>
+      </li>
+    </ol>
+    <div
+      v-else
+      class="sanstream-special-text"
+    >No searchterm applied.</div>
+  </div>
+</template>
+
+<script>
+import VueTypes from 'vue-types'
+import Button from '../elements/Button'
+export default {
+  components: { Button, },
+  name: 'AppliedSearchTerms',
+  props: {
+    searchTerms: VueTypes.arrayOf(VueTypes.string.isRequired).def([]),
+  },
+}
+</script>
+
+<style scoped>
+.search-terms li {
+  display: inline-block;
+}
+</style>
